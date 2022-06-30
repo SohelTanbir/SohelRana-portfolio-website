7b43331f648fe9627479dd9d998332b28931f23d
SohelTanbir <soheltanbir19@gmail.com>
2022-06-30 12:35:37 +0600

    modify design on  portfolio section and load portfolio from fake database api

---
+++ src/Data/Data.js
@@ -0,0 +1,66 @@
+const portfolio_demo = [
+    {
+        title:'Appri App Landing page',
+        img:'../images/portfolio/appri.png',
+        description:'Appri is a app landing page.it is  a full responsive website Templete.To make it I used only HTML and CSS',
+        code_link:'https://github.com/SohelTanbir/Appri-App-landing-page',
+        live_demo:'https://soheltanbir.github.io/Appri-App-landing-page/'
+    },
+    {    title:'Al Quran App',
+        img:'../images/portfolio/al-quran.png',
+        description:'Al Quran web application for muslim people.This is a amazing application with three mode to use it.I use React js for Front and use Free API for Back end',
+        code_link:'https://github.com/SohelTanbir/Vaid-tech/tree/Al-Quran',
+        live_demo:'https://melodic-bombolone-809e83.netlify.app/'
+    },
+    {
+        title:'Simple Text Editor',
+        img:'../images/portfolio/text-editor.png',
+        description:'This is a Simple WYSIWYG Text Editor. I used HTML, CSS and JavaScript for the project',
+        code_link:'https://github.com/SohelTanbir/Vaid-tech/tree/text-editor',
+        live_demo:'https://soheltanbir.github.io/Vaid-tech/text-editor.html'
+    },
+    {
+        title:'Mobile Repire IT',
+        img:'../images/portfolio/mobileCare-large.png',
+        description:'This is a Full stack single page web application with Dashboard.I used to make it React js, Node js, Express js, MongoDB and firebase.customer can repair their mobile phone by perches repair package from the application.',
+        code_link:'https://github.com/SohelTanbir/mobileCare',
+        live_demo:'https://mobile-care-3d874.web.app/"'
+    },
+    {
+        title:'Travel Agency',
+        img:'../images/portfolio/travel-agency -large.png',
+        description:"Travel agency is onlne Ticket Booking application.pasanger can buy ticket from the application and can see destination by google map.it's made by React js, firebase Authentication",
+        code_link:'https://github.com/SohelTanbir/Travel-agency-with-authentication',
+        live_demo:'https://travel-agency-68e4d.web.app/'
+    },
+    {
+        title:'E-Store ',
+        img:'../images/portfolio/estore.png',
+        description:"E-Store is an E-commerce Website.customer can buy their cloth from the website.To make it I used Only HTML and CSS",
+        code_link:'https://github.com/SohelTanbir/E-Store',
+        live_demo:'https://soheltanbir.github.io/E-Store/index.html'
+    },
+    {
+        title:'Finix Web Template ',
+        img:'../images/portfolio/finix.png',
+        description:"Finix is beautiful get appoint web Template .To make it I used only HTML and CSS",
+        code_link:'https://github.com/SohelTanbir/Finix',
+        live_demo:'https://soheltanbir.github.io/Finix/index.html'
+    },
+    {
+        title:'NewBiz Template ',
+        img:'../images/portfolio/nebize.png',
+        description:"NewBiz is beautiful Business solution web Template .To make it I used only HTML and CSS",
+        code_link:'https://github.com/SohelTanbir/NewBiz-webiste',
+        live_demo:'https://soheltanbir.github.io/NewBiz-webiste/'
+    },
+    {
+        title:'Noxia Web Template',
+        img:'../images/portfolio/noxia.png',
+        description:"Noxia is Service provided website, .To make it I used only HTML and CSS",
+        code_link:'https://github.com/SohelTanbir/Noxia-website',
+        live_demo:'https://soheltanbir.github.io/Noxia-website/'
+    },
+]
+
+export default portfolio_demo;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './Projects.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import developer from '../../images/portfolio/developer-cmmunity-samll.png';
-import repair from '../../images/portfolio/mobileCare-small.png';
-import appri from '../../images/portfolio/portfolio-1.png';
-import  portfolio from '../../images/portfolio/portfolio-1.png';
-import  travelAgency from '../../images/portfolio/travel-agency-small.png';
-import  finix from '../../images/portfolio/portfolio-2.png';
-import  winter from '../../images/portfolio/portfolio-3.png';
-import  newbiz from '../../images/portfolio/portfolio-4.png';
-import  noxia from '../../images/portfolio/portfolio-5.png';
-import  teamviewer from '../../images/portfolio/team viewer.png';
 import { faGithub, } from '@fortawesome/free-brands-svg-icons';
 import { faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons';
+import portfolio_demo from '../../Data/Data';
+
 
 const Projects = () => {
+
     return (
         <div className="projects">
            <div className="container">
@@ -23,159 +16,34 @@ const Projects = () => {
                    <h4>What have I done ?</h4>
                </div>
                <div className="row">
-                  <div className="project">
-                      <img src={appri} alt="developer"/>
+                  {
+                    portfolio_demo.map((portfolio, index)=>{
+                      return(
+                        <>
+                        <div className="project" key={index}>
+                      <img src={portfolio.img} alt="developer"/>
                       <div className="project-overlay">
                           <div className="project-info">
-                            <h3>Appri App Landing page</h3>
-                            <p>Appri is a app landing page.it's a full responsive website Templete.To make it I used only HTML and CSS</p>
+                            <h3>{portfolio.title}</h3>
+                            <p>{portfolio.description}</p>
                             <div className="live-demo">
-                                <a href="https://github.com/SohelTanbir/Appri-App-landing-page" target="_blank">
+                                <a href={portfolio.code_link} target="_blank" rel='noreferrer'>
                                 <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
                                 </a>
-                                <a href="https://soheltanbir.github.io/Appri-App-landing-page/" target="_blank">
+                                <a href={portfolio.live_demo} target="_blank" rel='noreferrer'>
                                 <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
                                 </a>
                             </div>
                           </div>
                       </div>
                   </div>
-                  <div className="project">
-                      <img src={repair} alt="repairIT"/>  
-                      <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>Mobile Repire IT</h3>
-                            <p>it's a Full stack single page web application with Dashboard.I used to make it React js, Node js, Express js, MongoDB and firebase.customer can repair their mobile phone by perches repair package from the application.</p>
-                            <div className="live-demo">
-                                <a href="https://github.com/SohelTanbir/mobileCare" target="_blank">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="https://mobile-care-3d874.web.app/" target="_blank">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="project">
-                        <img src={developer} alt="developer"/>
-                        <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>Developer Community</h3>
-                            <p>Developer Community is Blog Single page Applicaiton.it's maked by React js.Developer can write blog and published video Tutorial from the application.</p>
-                            <div className="live-demo">
-                                <a href="https://github.com/SohelTanbir/developers-community" target="_blank">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="https://competent-mccarthy-3eb620.netlify.app/" target="_blank">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="project">
-                        <img src={travelAgency} alt="repairIT"/>
-                        <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>Travel Agency</h3>
-                            <p>Travel agency is onlne Ticket Booking application.pasanger can buy ticket from the application and can see destination by google map.it's made by React js, firebase Authentication</p>
-                            <div className="live-demo">
-                                <a href="https://github.com/SohelTanbir/Travel-agency-with-authentication">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="https://github.com/SohelTanbir/Travel-agency-with-authentication">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="project">
-                        <img src={winter} alt="repairIT"/>
-                        <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>E-Store </h3>
-                            <p>E-Store is an E-commerce Website.customer can buy their cloth from the website.To make it I used Only HTML and CSS</p>
-                            <div className="live-demo">
-                                <a href="https://github.com/SohelTanbir/E-Store" target="_blank">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="https://soheltanbir.github.io/E-Store/index.html" target="_blank">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="project">
-                        <img src={finix} alt="repairIT"/>
-                        <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>Finix Web Template</h3>
-                            <p>Finix is beautiful get appoint web Template .To make it I used only HTML and CSS</p>
-                            <div className="live-demo">
-                                <a href="#">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="#">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="project">
-                        <img src={newbiz} alt="repairIT"/>
-                        <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>Appri App Landing page</h3>
-                            <p>Appri is a app landing page.it's a full responsive website Templete.To make it I used only HTML and CSS</p>
-                            <div className="live-demo">
-                                <a href="https://github.com/SohelTanbir/NewBiz-webiste" target="_blank">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="https://soheltanbir.github.io/NewBiz-webiste/" target="_blank">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="project">
-                        <img src={noxia} alt="repairIT"/>
-                        <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>Noxia Web Template</h3>
-                            <p>Noxia is Service provided website, .To make it I used only HTML and CSS</p>
-                            <div className="live-demo">
-                                <a href="https://github.com/SohelTanbir/Noxia-website" target="_blank">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="https://soheltanbir.github.io/Noxia-website/" target="_blank">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="project">
-                        <img src={teamviewer} alt="repairIT"/>
-                        <div className="project-overlay">
-                          <div className="project-info">
-                            <h3>Team Viewer Application</h3>
-                            <p>eam Viewer single page Application.user can view all information of player Team.To make it I used React js</p>
-                            <div className="live-demo">
-                                <a href="" target="_blank">
-                                <button>Code link <FontAwesomeIcon icon={faGithub}/></button>
-                                </a>
-                                <a href="https://amazing-bassi-bd1eff.netlify.app/" target="_blank">
-                                <button>Live demo <FontAwesomeIcon icon={faExternalLinkAlt}/></button>
-                                </a>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
+                        </>
+                      ) 
+                    } )
+                  }
+                <div className="load-more-portfolio">
+                <button className="comonBtn">Show more</button>
+                </div>
                </div>
            </div>
         </div>
